4946ae9701bc6710d65a58b6de207334ba24a2c1
Andrew Morton <andrewmorton@gocardless.com>
2023-04-19 10:03:58 +0100

    Add streaming interface to the integration spec
    Add exercising of the streaming interface to the integration spec.
    
    For this we primarily focus on verifying the streaming API provides the same
    output for a given input.

--- spec/bucket_store_integration_spec.rb
+++ spec/bucket_store_integration_spec.rb
@@ -4,6 +4,8 @@ require "spec_helper"
 
 require "aws-sdk-s3"
 
+require "tempfile"
+
 RSpec.describe BucketStore, :integration do
   before do
     # Setup AWS connectivity to minio
@@ -31,7 +33,110 @@ RSpec.describe BucketStore, :integration do
         end
       end
 
-      it "has a consistent interface" do
+      context "when handling streaming uploads / downloads" do
+        let(:temp_filename) { "temp-file" }
+        let(:buffer_filename) { "in-memory-buffer" }
+        let(:input_temp_file) do
+          Tempfile.new.tap do |file|
+            file.write(Random.base64(1024))
+            file.rewind
+          end
+        end
+        let(:output_temp_file) do
+          Tempfile.new
+        end
+
+        let(:input_in_memory_buffer) do
+          StringIO.new(Random.base64(1024))
+        end
+        let(:output_in_memory_buffer) do
+          StringIO.new
+        end
+
+        before do
+          described_class.for("#{base_bucket_uri}/#{temp_filename}").
+            stream.
+            upload!(file: input_temp_file)
+          input_temp_file.rewind
+
+          described_class.for("#{base_bucket_uri}/#{buffer_filename}").
+            stream.
+            upload!(file: input_in_memory_buffer)
+          input_in_memory_buffer.rewind
+        end
+
+        after do
+          input_temp_file.close
+          output_temp_file.close
+        end
+
+        it "all file are listable on the store" do
+          expect(described_class.for(base_bucket_uri.to_s).list.to_a.size).to eq(2)
+        end
+
+        it "files are deletable on the store" do
+          described_class.for(base_bucket_uri.to_s).list.each do |key|
+            described_class.for(key).delete!
+          end
+          expect(described_class.for(base_bucket_uri.to_s).list.to_a.size).to eq(0)
+        end
+
+        it "files are downloadable from the store" do
+          described_class.for("#{base_bucket_uri}/#{temp_filename}").
+            stream.
+            download(file: output_temp_file)
+          output_temp_file.rewind
+
+          expect(output_temp_file.read).to eq(input_temp_file.read)
+
+          described_class.for("#{base_bucket_uri}/#{buffer_filename}").
+            stream.
+            download(file: output_in_memory_buffer)
+          output_in_memory_buffer.rewind
+
+          expect(output_in_memory_buffer.read).to eq(input_in_memory_buffer.read)
+        end
+
+        context "when the files are binary" do
+          let(:input_temp_file) do
+            Tempfile.new.binmode.tap do |file|
+              file.write(Random.bytes(1024))
+              file.rewind
+            end
+          end
+          let(:output_temp_file) do
+            Tempfile.new.binmode
+          end
+
+          let(:input_in_memory_buffer) do
+            StringIO.new.binmode.tap do |buffer|
+              buffer.write(Random.bytes(1024))
+              buffer.rewind
+            end
+          end
+          let(:output_in_memory_buffer) do
+            StringIO.new.binmode
+          end
+
+          it "files are downloadable from the store" do
+            described_class.for("#{base_bucket_uri}/#{temp_filename}").
+              stream.
+              download(file: output_temp_file)
+            output_temp_file.rewind
+
+            expect(output_temp_file.read).to eq(input_temp_file.read)
+
+            described_class.for("#{base_bucket_uri}/#{buffer_filename}").
+              stream.
+              download(file: output_in_memory_buffer)
+            output_in_memory_buffer.rewind
+
+            expect(output_in_memory_buffer.read).to eq(input_in_memory_buffer.read)
+          end
+        end
+      end
+
+      it "has a consistent interface for string uploads / downloads" do
         # Write 201 files
         file_list = []
         201.times do |i|
@@ -77,7 +182,6 @@ RSpec.describe BucketStore, :integration do
     end
   end
 
-  # We don't test GCS as there's no sensible way of running a local simulator
   include_examples "adapter integration", "inmemory://bucket"
   include_examples "adapter integration", "disk://bucket"
   include_examples "adapter integration", "s3://bucket"
